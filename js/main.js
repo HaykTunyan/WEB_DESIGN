9c5ba6bae6ba263282259c5f1f066325affaa968
HaykTunyan <hayktunyan1994@gmail.com>
2018-02-19 23:51:19 +0400

    Materialize Web_page

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// customer file
+
+$(document).ready(function(){
+    $('.carousel').carousel();
+});
+
+//
+$(document).ready(function(){
+    $('.slider').slider();
+});
+
+//
+$(document).ready(function(){
+    $('.parallax').parallax();
+});
+
+//
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+});
+
+
